3138ec6f2e56b7ee8bfc0027bd33f4c41eb8bffa
jeetbafna <jeetanil97@gmail.com>
2017-02-26 15:49:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admin/serversocket/DataDisplay.java
@@ -0,0 +1,8 @@
+package com.example.admin.serversocket;
+
+/**
+ * Created by admin on 26/02/2017.
+ */
+public interface DataDisplay {
+    void Display(String message);
+}
---
+++ app/src/main/java/com/example/admin/serversocket/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.admin.serversocket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements DataDisplay{
+    TextView serverMessage;
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        serverMessage=(TextView)findViewById(R.id.textView);
+    }
+    public void connect(View view)
+    {
+        MyServer server=new MyServer();
+        server.setEventListener(this);
+        server.startListening();
+
+    }
+    public void Display(String message)
+    {
+        serverMessage.setText(""+message);
+
+    }
+
+}
